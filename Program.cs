379c1fbbaa624f5bfb199f846be0dfd2d77e10c8
Amir Alnomani <alnomani@protonmail.com>
2023-12-21 15:20:05 +0100

    Extracted to class and added memoitization.

--- Program.cs
+++ Program.cs
@@ -9,60 +9,70 @@
     nothing.
     Alice’s goal: get the most.
 */
+
 internal class Program
 {
     private static void Main(string[] args)
     {
-
-        displayCoinStrategyFor([5, 10, 25, 10]);
-        displayCoinStrategyFor([1, 3, 6, 3, 1, 3]);
-
-        void displayCoinStrategyFor(int[] inputCoinList){
-            GameStrategy output = getCoinStrategy(inputCoinList);
-            output.strategy.Reverse();
-            Console.WriteLine($"{output.aSum} {output.bSum} [{string.Join(", ", output.strategy)}]");
-        }
-
-        GameStrategy getCoinStrategy(int[] coinValueList){
-            if (coinValueList.Length == 1) return new GameStrategy(0, coinValueList[0]);
-            GameStrategy rightSums = getCoinStrategy(coinValueList[1..]);
-            GameStrategy leftSums = getCoinStrategy(coinValueList[..(coinValueList.Length - 1)]);
-            if (coinValueList.Length % 2 == 0){
-                return alicesTurn(coinValueList, rightSums, leftSums);
-            }else{
-                return bobsTurn(coinValueList, rightSums, leftSums);
-            }
-
-        }
-        
-        GameStrategy alicesTurn(int[] coinValueList, GameStrategy rightSums, GameStrategy leftSums)
-        {
-            int rightCoin = coinValueList[coinValueList.Length - 1];
-            rightSums.aSum += coinValueList[0];
-            leftSums.aSum += rightCoin;
-            GameStrategy result = rightSums.aSum > leftSums.aSum ? rightSums : leftSums;
-            int highestCoin = rightSums.aSum > leftSums.aSum ? coinValueList[0] : rightCoin;
-            result.strategy.Add(highestCoin);
-            return result;
-        }
-
-        GameStrategy bobsTurn(int[] coinValueList, GameStrategy rightSums, GameStrategy leftSums){
-            rightSums.bSum += coinValueList[0];
-            leftSums.bSum += coinValueList[coinValueList.Length - 1];
-            return  rightSums.bSum > leftSums.bSum ? rightSums : leftSums;
-        }
+        CoinGameSolver.DisplayCoinStrategyFor([5, 10, 25, 10]);
+        CoinGameSolver.DisplayCoinStrategyFor([1, 3, 6, 3, 1, 3]);
     }
 }
 
-public struct GameStrategy
+public struct GameStrategy(int aSum, int bSum)
 {
-    public int aSum;
-    public int bSum;
-    public List<int> strategy;
-    public GameStrategy(int aSum, int bSum)  
-    {  
-        this.aSum = aSum;
-        this.bSum = bSum;
-        this.strategy = new List<int>();
-    } 
+    public int aSum = aSum;
+    public int bSum = bSum;
+    public List<int> strategy = [];
+}
+
+public static class CoinGameSolver
+{
+    private static readonly Dictionary<int[], GameStrategy> numberNames = [];
+
+    public static void DisplayCoinStrategyFor(int[] inputCoinList)
+    {
+        GameStrategy output = GetCoinStrategy(inputCoinList);
+        output.strategy.Reverse();
+        Console.WriteLine($"{output.aSum} {output.bSum} [{string.Join(", ", output.strategy)}]");
+    }
+
+    private static GameStrategy GetCoinStrategy(int[] coinValueList)
+    {
+        if (numberNames.TryGetValue(coinValueList, out GameStrategy value))
+            return value;
+        if (coinValueList.Length == 1)
+            return new GameStrategy(0, coinValueList[0]);
+        GameStrategy right = GetCoinStrategy(coinValueList[1..]);
+        GameStrategy left = GetCoinStrategy(coinValueList[..^1]);
+
+        if (coinValueList.Length % 2 == 0)
+        {
+            return AlicesTurn(coinValueList, right, left);
+        }
+        else
+        {
+            return BobsTurn(coinValueList, right, left);
+        }
+    }
+
+    private static GameStrategy AlicesTurn(int[] coinValues, GameStrategy right, GameStrategy left)
+    {
+        right.aSum += coinValues[0];
+        left.aSum += coinValues[^1];
+        GameStrategy result = right.aSum > left.aSum ? right : left;
+        int highestCoin = right.aSum > left.aSum ? coinValues[0] : coinValues[^1];
+        result.strategy.Add(highestCoin);
+        numberNames.Add(coinValues, result);
+        return result;
+    }
+
+    private static GameStrategy BobsTurn(int[] coinValueList, GameStrategy right, GameStrategy left)
+    {
+        right.bSum += coinValueList[0];
+        left.bSum += coinValueList[^1];
+        GameStrategy result = right.bSum > left.bSum ? right : left;
+        numberNames.Add(coinValueList, result);
+        return result;
+    }
 }
--- obj/Debug/net8.0/Exercises.AssemblyInfo.cs
+++ obj/Debug/net8.0/Exercises.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercises")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fccabb014bee018e07f1ae4eaee892c83f749dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercises")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercises")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
